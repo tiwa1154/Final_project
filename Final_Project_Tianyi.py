b15042bab18b320ac2b0a175b2015439ccb6f3e8
tianywan819 <57418894+tianywan819@users.noreply.github.com>
2021-11-30 11:57:54 -0500

    Update Final_Project_Tianyi.py

--- Final_Project_Tianyi.py
+++ Final_Project_Tianyi.py
@@ -69,6 +69,15 @@ import pandas as pd
 # import sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import accuracy_score, confusion_matrix, r2_score
 
 #%%
 filepath = os.path.join(os.getcwd())
@@ -86,17 +95,93 @@ credit.head(n = 3)
 # To get the wide table
 credit['status'] = np.where((credit['STATUS'] == '2') | (credit['STATUS'] == '3' )| (credit['STATUS'] == '4' )| (credit['STATUS'] == '5'), 1, 0) # define > 60 days past-due
 
-credit_piv = credit.pivot(index = 'ID', columns = 'MONTHS_BALANCE', values = 'STATUS')
+df = credit.pivot(index = 'ID', columns = 'MONTHS_BALANCE', values = 'STATUS')
 #%%
-credit_piv.head(n = 3)
+df.head(n = 3)
 #%%
-# Inner Join two data sets with intersection ID.
-df = pd.merge(app, credit_piv, how="inner", on=["ID", "ID"])
-df.head(3)
-# len(df)
+def data_cleansing(data):
+    # Adding number of family members with number of children to get overall family members.
+    data['CNT_FAM_MEMBERS'] = data['CNT_FAM_MEMBERS'] + data['CNT_CHILDREN']
+    dropped_cols = ['FLAG_MOBIL', 'FLAG_WORK_PHONE', 'FLAG_PHONE',
+       'FLAG_EMAIL','OCCUPATION_TYPE','CNT_CHILDREN']
+    data = data.drop(dropped_cols, axis = 1)
 
-# %%
-# Count number of different status.
+    data['DAYS_BIRTH'] = np.abs(data['DAYS_BIRTH']/365)
+    data['DAYS_EMPLOYED'] = data['DAYS_EMPLOYED']/365 
+
+    # housing_type = {'House / apartment' : 'House / apartment',
+    #                'With parents': 'With parents',
+    #                 'Municipal apartment' : 'House / apartment',
+    #                 'Rented apartment': 'House / apartment',
+    #                 'Office apartment': 'House / apartment',
+    #                 'Co-op apartment': 'House / apartment'}
+              
+    # income_type = {'Commercial associate':'Working',
+    #               'State servant':'Working',
+    #               'Working':'Working',
+    #               'Pensioner':'Pensioner',
+    #               'Student':'Student'}
+    # education_type = {'Secondary / secondary special':'secondary',
+    #                  'Lower secondary':'secondary',
+    #                  'Higher education':'Higher education',
+    #                  'Incomplete higher':'Higher education',
+    #                  'Academic degree':'Academic degree'}
+    # family_status = {'Single / not married':'Single',
+    #                  'Separated':'Single',
+    #                  'Widow':'Single',
+    #                  'Civil marriage':'Married',
+    #                 'Married':'Married'}
+    # data['NAME_HOUSING_TYPE'] = data['NAME_HOUSING_TYPE'].map(housing_type)
+    # data['NAME_INCOME_TYPE'] = data['NAME_INCOME_TYPE'].map(income_type)
+    # data['NAME_EDUCATION_TYPE']=data['NAME_EDUCATION_TYPE'].map(education_type)
+    # data['NAME_FAMILY_STATUS']=data['NAME_FAMILY_STATUS'].map(family_status)
+    return data
+
+cleansed_app = data_cleansing(app)
+#%%
+def feature_engineering_target(data):
+    good_or_bad = []
+    for index, row in data.iterrows():
+        paid_off = row['pay_off']
+        over_1 = row['overdue_1-29']
+        over_30 = row['overdue_30-59']
+        over_60 = row['overdue_60-89'] 
+        over_90 = row['overdue_90-119']
+        over_120 = row['overdue_120-149'] + row['overdue_over_150']
+        no_loan = row['no_loan']
+            
+        overall_overdues = over_1+over_30+over_60+over_90+over_120    
+            
+        if overall_overdues == 0:
+            if paid_off >= no_loan or paid_off <= no_loan:
+                good_or_bad.append(1)
+            elif paid_off == 0 and no_loan == 1:
+                good_or_bad.append(1)
+        
+        elif overall_overdues != 0:
+            if paid_off > overall_overdues:
+                good_or_bad.append(1)
+            elif paid_off <= overall_overdues:
+                good_or_bad.append(0)
+        
+        elif paid_off == 0 and no_loan != 0:
+            if overall_overdues <= no_loan or overall_overdues >= no_loan:
+                good_or_bad.append(0)
+
+        else:
+            good_or_bad.append(1)
+                
+        
+    return good_or_bad
+# target set to 1 is good client b/c the gap between # of overdues and payoff or no loan is significant, otherwise, target == 0
+
+#%%
+
+# Inner Join two data sets with intersection ID.
+# df = pd.merge(app, df, how="inner", on=["ID", "ID"])
+# df.head(3)
+
+#%%
 df['pay_off'] = df[df.iloc[:,18:79] == 'C'].count(axis = 1)
 df['overdue_1-29'] = df[df.iloc[:,18:79] == '0'].count(axis = 1)
 df['overdue_30-59'] = df[df.iloc[:,18:79] == '1'].count(axis = 1)
@@ -105,17 +190,190 @@ df['overdue_90-119'] = df[df.iloc[:,18:79] == '3'].count(axis = 1)
 df['overdue_120-149'] = df[df.iloc[:,18:79] == '4'].count(axis = 1)
 df['overdue_over_150'] = df[df.iloc[:,18:79] == '5'].count(axis = 1)
 df['no_loan'] = df[df.iloc[:,18:79] == 'X'].count(axis = 1)
+df['ID'] = df.index
 #%%
-# df.head(3)
-# %%
-# Subset dataframe 
-dfs = df.iloc[:, np.r_[0:18, 79:87]]
-dfs.head(3)
-# %%
-dfs.to_csv('subset.csv')
+df.head(10)
+
 #%%
-# Check the data type of each column. 
-dfs.iloc[:,0:26].info(verbose=True)
+target = pd.DataFrame()
+target['ID'] = df.index
+target['pay_off'] = df['pay_off'].values
+target['#_of_overdues'] = df['overdue_1-29'].values+ df['overdue_30-59'].values + df['overdue_60-89'].values +df['overdue_90-119'].values+df['overdue_120-149'].values +df['overdue_over_150'].values
+target['no_loan'] = df['no_loan'].values
+target['target'] = feature_engineering_target(df)
+df_merge = cleansed_app.merge(target, how="inner", on="ID")
+df_merge.head(10)
+df_merge.describe()
+
+#%%
+# Investigate all the elements whithin each Feature 
+
+for column in df_merge:
+    unique_vals = np.unique(df_merge[column])
+    nr_values = len(unique_vals)
+    if nr_values < 12:
+        print('The number of values for feature {} :{} -- {}'.format(column, nr_values,unique_vals))
+    else:
+        print('The number of values for feature {} :{}'.format(column, nr_values))
+
+#%%
+# Checking for null values
+df_merge.isnull().sum()
+
+#%%
+# Print out list of columns
+df_merge.columns
+
+#%%
+df_merge2 = df_merge[['CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY',
+       'AMT_INCOME_TOTAL', 'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE',
+       'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE', 'DAYS_BIRTH',
+       'DAYS_EMPLOYED', 'CNT_FAM_MEMBERS', 'pay_off', '#_of_overdues',
+       'no_loan', 'target']]
+# Visualize the data using seaborn Pairplots
+g = sns.pairplot(df_merge2, hue = 'target', diag_kws={'bw': 0.2})
+# observe good relationship between total income and pay off, days employed and pay off, age and no loan/pay off, 
+#%%
+# Investigate all the features by our y
+features = ['CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY',
+       'AMT_INCOME_TOTAL', 'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE',
+       'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE', 'DAYS_BIRTH',
+       'DAYS_EMPLOYED', 'CNT_FAM_MEMBERS', 'pay_off', '#_of_overdues',
+       'no_loan']
+
+
+for f in features:
+    plt.figure()
+    ax = sns.countplot(x=f, data=df_merge2, hue = 'target', palette="Set1")
+# More famle than male in general, more applicants own properties and good applicants have fewer property counts. more applicants fall under working catagory, have secondary degree, married, have 2 children. good candidates have higher pay off time and usually pay of within a month. Bad candidates have higher # of overdue.
+
+#%%
+df_merge2.head()
+#%%
+# convert catagorical variables to numeric
+numerical_df_merge2 = pd.get_dummies(df_merge2, columns = ['NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE', 'CODE_GENDER','FLAG_OWN_CAR', 'FLAG_OWN_REALTY'])
+numerical_df_merge2.head()
+
+#%%
+# Scaling columns between 0 and 1 for faster training
+scale_vars = ['AMT_INCOME_TOTAL','DAYS_BIRTH','DAYS_EMPLOYED']
+scaler = MinMaxScaler()
+numerical_df_merge2[scale_vars] = scaler.fit_transform(numerical_df_merge2[scale_vars])
+numerical_df_merge2.head()
+
+#%%
+# Split data
+X = numerical_df_merge2.drop('target', axis=1).values
+y = numerical_df_merge2['target'].values 
+print('X shape: {}'.format(np.shape(X)))
+print('y shape: {}'.format(np.shape(y)))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, test_size=0.2, random_state=0)
+
+#%%
+# Decision tree
+dt = DecisionTreeClassifier(criterion='entropy', max_depth=2, random_state=1)
+dt.fit(X_train, y_train)
+
+#%%
+from sklearn.tree import export_graphviz
+
+dot_data = tree.export_graphviz(dt, out_file=None, 
+    feature_names=numerical_df_merge2.drop('target', axis=1).columns,    
+    class_names=numerical_df_merge2['target'].unique().astype(str),  
+    filled=True, rounded=True,  
+    special_characters=True)
+graph = graphviz.Source(dot_data)
+graph
+
+#%%
+
+#%%
+# xtarget = df_merge[df_merge.drop('target',axis = 1).columns]
+# ytarget = df_merge['target']
+# xtrain, xtest, ytrain, ytest = train_test_split(xtarget,ytarget, train_size = 0.8,stratify=ytarget, random_state = 0)
+
+#%%
+from sklearn.tree import DecisionTreeClassifier
+# Import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix 
+from sklearn.metrics import classification_report
+# Instantiate dtree
+# dtree_admit1 = DecisionTreeClassifier(max_depth=2, random_state=1)
+# # Fit dt to the training set
+# dtree_admit1.fit(xtrain,ytrain)
+# # Predict test set labels
+# y_test_pred = dtree_admit1.predict(xtest)
+# # Evaluate test-set accuracy
+# print(accuracy_score(ytest, y_test_pred))
+# print(confusion_matrix(ytest, y_test_pred))
+# print(classification_report(ytest, y_test_pred))
+# # %%
+# # Subset dataframe 
+# dfs = df.iloc[:, np.r_[0:18, 79:87]]
+# dfs.head(10)
+# test classification dataset
+from sklearn.datasets import make_classification
+# define dataset
+X, y = make_classification(n_samples=36457, n_features=20, n_informative=8, n_redundant=5, random_state=3)
+# summarize the dataset
+print(X.shape, y.shape)
+
+#%%
+from numpy import mean
+from numpy import std
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+# define the model
+model = RandomForestClassifier()
+# evaluate the model
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+# report performance
+print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+# # Check the data type of each column. 
+# dfs.iloc[:,0:26].info(verbose=True)
+
+#%%
+def get_dataset():
+	X, y = make_classification(n_samples=36457, n_features=20, n_informative=8, n_redundant=5, random_state=3)
+	return X, y
+ 
+# get a list of models to evaluate
+def get_models():
+	models = dict()
+	# explore number of features from 1 to 7
+	for i in range(1,8):
+		models[str(i)] = RandomForestClassifier(max_features=i)
+	return models
+ 
+# evaluate a given model using cross-validation
+def evaluate_model(model, X, y):
+	# define the evaluation procedure
+	cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+	# evaluate the model and collect the results
+	scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+	return scores
+ 
+# define dataset
+X, y = get_dataset()
+# get the models to evaluate
+models = get_models()
+# evaluate the models and store results
+results, names = list(), list()
+for name, model in models.items():
+	# evaluate the model
+	scores = evaluate_model(model, X, y)
+	# store the results
+	results.append(scores)
+	names.append(name)
+	# summarize the performance along the way
+	print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+# plot model performance for comparison
+plt.boxplot(results, labels=names, showmeans=True)
+plt.show()
 # %%
 # Write a summary function to have a glance of the numeric part
 # of the data set
@@ -129,7 +387,7 @@ def var_summary(x):
 #%%
 # Select the numeric variables from app. 
 print("Summary of Numeric Varible")
-df_nu = dfs.select_dtypes([np.number]) 
+df_nu = df_merge.select_dtypes([np.number]) 
 df_nu.apply(var_summary).T
 # Everything looks fine. 
 
@@ -139,10 +397,27 @@ df_nu.apply(var_summary).T
 
 #%%
 # Make sum of the total loan based on subset of dataframe, easier for visualization
-df_sub = pd.read_csv('subset.csv')
-df_sub['sum_column']= df_sub.iloc[:,20:26].sum(axis=1)
-df_sub.head()
+# df_sub = pd.read_csv('subset.csv')
+# df_sub['sum_column']= df_sub.iloc[:,20:26].sum(axis=1)
+# df_sub.head()
 # higher sum means more days overdue
+
+#%%
+df_sub.groupby('OCCUPATION_TYPE')['OCCUPATION_TYPE'].count()
+
+#%%
+df_sub_null = df_sub.dropna(axis=0)
+df_sub_null.isnull().any()
+
+# #%%
+# df_sub_clean['FLAG_OWN_CAR'] = pd.factorize(df_sub_clean['FLAG_OWN_CAR'])[0]
+# df_sub_clean['FLAG_OWN_REALTY'] = pd.factorize(df_sub_clean['FLAG_OWN_REALTY'])[0]
+# df_sub_clean['NAME_INCOME_TYPE'] = pd.factorize(df_sub_clean['NAME_INCOME_TYPE'])[0]
+# df_sub_clean['NAME_EDUCATION_TYPE'] = pd.factorize(df_sub_clean['NAME_EDUCATION_TYPE'])[0]
+# df_sub_clean['NAME_FAMILY_STATUS'] = pd.factorize(df_sub_clean['NAME_FAMILY_STATUS'])[0]
+# df_sub_clean['NAME_HOUSING_TYPE'] = pd.factorize(df_sub_clean['NAME_HOUSING_TYPE'])[0]
+# df_sub_clean['OCCUPATION_TYPE'] = pd.factorize(df_sub_clean['OCCUPATION_TYPE'])[0]
+# df_sub_clean.dtypes
 
 #%%
 # total annual income vs. debt overdue period
@@ -201,6 +476,10 @@ plt.show()
 # %%
 # df_sub.plot(x=kids, y=debt_sum, kind="bar")
 # plt.show()
+
+
+#%%
+cleansed_df_sub = data_cleansing(df_sub)
 
 #%%
 df_sub.NAME_FAMILY_STATUS.value_counts()
@@ -291,5 +570,26 @@ print(type(yclient))
 from pandas.plotting import scatter_matrix
 scatter_matrix(xclient, alpha = 0.2, figsize = (7, 7), diagonal = 'hist')
 plt.show()
+
+# %%
+# clustering
+from numpy import where
+from sklearn.datasets import make_classification
+from matplotlib import pyplot
+# define dataset
+X, y = make_classification(n_samples=36457, n_subdf_sub=9, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=10)
+# create scatter plot for samples from each class
+for class_value in range(2):
+	# get row indexes for samples with this class
+	row_ix = where(y == class_value)
+	# create scatter of these samples
+	pyplot.scatter(X[row_ix, 0], X[row_ix, 1])
+# show the plot
+pyplot.show()
+# %%
+x = df[df.drop('target', axis = 1).columns]
+y = df['target']
+xtrain, xtest, ytrain, ytest = train_test_split(x,y, train_size = 0.8, random_state = 0)
+
 
 # %%
