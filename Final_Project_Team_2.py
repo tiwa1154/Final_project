57ce1d650845b5f58f346d401e351c8dc8cd1d5a
Baldur Hua <Baldur738@gwmail.gwu.edu>
2021-12-02 02:41:37 -0500

    Revert "Data frame refined and Xgboost model added"
    This reverts commit 8b50b7f715b2e886b774e98fab78e65bfb592cff.

---
+++ Final_Project_Team_2.py
@@ -0,0 +1,136 @@
+#%%
+# Sharing some hot keys to help with editing. Happy Coding, team.
+#
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+# To undo 'ctrl + Z'
+# To wrap for long text 'alt + Z'
+# To edit multiple line 'ctrl + alt + arrow'
+# To comment out code 'ctrl + /'
+# To edit text with the same content 'ctrl + shift + L'
+#%%[markdown]
+## DAT - 6103 - Data Mining - Final Project
+#
+# Team 2 Members: 
+# *  Tianyi Wang
+# *  Baldur Hua
+# *  Chiemeziem Oguayo
+# *  Meg Elliott Fitzgerald
+# %%[markdown]
+## Introduction
+# The average American is over $5,000 in credit card debt, a figure that’s back on the rise now that household spending has recovered from the 2019 pandemic-induced lull. This exceeds the average amount of credit card debt in any other country, and will likely only increase as times goes on if current trends continue. With so many of us frequently relying on credit cards, it behooves us to understand the patterns and profiles associated with credit card usage. Armed with this knowledge, we are able to recognize when certain characteristics make individuals more likely to accrue large balances or default on payments. For instance, based on data collected by Shift, on average men, individuals between the ages of 45-54, Generation X’ers, and those making more than $160,000 per year have the highest amount of credit card debt. By analyzing our dataset we hope to develop multi-dimensional profiles of credit card customers, and then explore whether these profiles are helpful in predicting credit card usage and payment behavior. In short, we hope to answer the following questions:
+#
+#   1. Can we unveil the relationship between individuals’ financial condition and their personal condition?
+# 
+#   2. Most importantly, can we develop customer profiles using clustering based on applicant data in order to predict future account behavior at the end?
+# 
+# * [Data Source](https://www.kaggle.com/rikdifos/credit-card-approval-prediction)
+# 
+# * [GitHub Link](https://www.kaggle.com/rikdifos/credit-card-approval-prediction)
+
+#%%[markdown]
+# Data Description (Application.csv):
+# * ID:	Client               number	
+# * CODE_GENDER:	         Gender	
+# * FLAG_OWN_CAR:	         Is there a car	
+# * FLAG_OWN_REALTY:	     Is there a property	
+# * CNT_CHILDREN:	         Number of children	
+# * AMT_INCOME_TOTAL:	     Annual income	
+# * NAME_INCOME_TYPE:	     Income category	
+# * NAME_EDUCATION_TYPE:	 Education level	
+# * NAME_FAMILY_STATUS:	     Marital status	
+# * NAME_HOUSING_TYPE:	     Way of living	
+# * DAYS_BIRTH	Birthday:	 
+#                            Count backwards from current day (0),  -1 means yesterday
+# * DAYS_EMPLOYED:	         Start date of employment	Count backwards from current day(0). If positive, it means the person currently unemployed.
+# * FLAG_MOBIL:	             Is there a mobile phone	
+# * FLAG_WORK_PHONE:	     Is there a work phone	
+# * FLAG_PHONE:	             Is there a phone	
+# * FLAG_EMAIL:	             Is there an email	
+# * OCCUPATION_TYPE:	     Occupation	
+# * CNT_FAM_MEMBERS:	     Family size	
+# 
+# Data Decription (Credit.csv)	
+# * ID:	               Client number	
+# * MONTHS_BALANCE:	   The month of the extracted data is the starting point, backwards, 0 is the current month, -1 is the previous month, and so on
+# * STATUS:	           0: 1-29 days past due 1: 30-59 days past due 2: 60-89 days overdue 3: 90-119 days overdue 4: 120-149 days overdue 5: Overdue or bad debts, write-offs for more than 150 days C: paid off that month X: No loan for the month
+
+# %%[markdown]
+## Preprocessing & EDA
+# In preprocessing part, we will clean the data set, making sure that they are in the right shape(eg. join data sets, remove(replace) NA values, 
+# correct data type, etc)
+#
+# In EDA part, a detailed summary of the data will be presented, along with graphs and test, preparing for the later model building 
+# proccess. 
+# %%
+import os
+import numpy as np
+import pandas as pd
+# import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+filepath = os.path.join(os.getcwd())
+app = pd.read_csv(os.path.join(filepath, "application.csv"))
+app.head(n = 3)
+
+#%%
+filepath2 = os.path.join(os.getcwd())
+credit = pd.read_csv(os.path.join(filepath2, "credit.csv"))
+credit.head(n = 3)
+
+#%%
+# Check the discussion here: https://www.kaggle.com/rikdifos/credit-card-approval-prediction/discussion/119320
+
+# To get the wide table
+credit['status'] = np.where((credit['STATUS'] == '2') | (credit['STATUS'] == '3' )| (credit['STATUS'] == '4' )| (credit['STATUS'] == '5'), 1, 0) # define > 60 days past-due
+
+credit_piv = credit.pivot(index = 'ID', columns = 'MONTHS_BALANCE', values = 'STATUS')
+#%%
+credit_piv.head(n = 3)
+#%%
+# Inner Join two data sets with intersection ID.
+df = pd.merge(app, credit_piv, how="inner", on=["ID", "ID"])
+df.head(3)
+# len(df)
+
+# %%
+# Count number of different status.
+df['pay_off'] = df[df.iloc[:,18:79] == 'C'].count(axis = 1)
+df['overdue_1-29'] = df[df.iloc[:,18:79] == '0'].count(axis = 1)
+df['overdue_30-59'] = df[df.iloc[:,18:79] == '1'].count(axis = 1)
+df['overdue_60-89'] = df[df.iloc[:,18:79] == '2'].count(axis = 1)
+df['overdue_90-119'] = df[df.iloc[:,18:79] == '3'].count(axis = 1)
+df['overdue_120-149'] = df[df.iloc[:,18:79] == '4'].count(axis = 1)
+df['overdue_over_150'] = df[df.iloc[:,18:79] == '5'].count(axis = 1)
+df['no_loan'] = df[df.iloc[:,18:79] == 'X'].count(axis = 1)
+#%%
+# df.head(3)
+# %%
+# Subset dataframe 
+dfs = df.iloc[:, np.r_[0:18, 79:87]]
+dfs.head(3)
+# %%
+#%%
+# Check the data type of each column. 
+dfs.iloc[:,0:26].info(verbose=True)
+# %%
+# Write a summary function to have a glance of the numeric part
+# of the data set
+def var_summary(x):
+    return pd.Series([x.count(), x.isnull().sum(), x.sum(), x.mean(), x.median(), x.std(), x.var(), x.min(), 
+        x.quantile(0.01), x.quantile(0.05), x.quantile(0.10),x.quantile(0.25),x.quantile(0.50),x.quantile(0.75), 
+                              x.quantile(0.90),x.quantile(0.95), x.quantile(0.99),x.max()], 
+                     
+                  index = ['N', 'NMISS', 'SUM', 'MEAN','MEDIAN', 'STD', 'VAR', 'MIN', 'P1', 
+                               'P5' ,'P10' ,'P25' ,'P50' ,'P75' ,'P90' ,'P95' ,'P99' ,'MAX'])
+#%%
+# Select the numeric variables from app. 
+print("Summary of Numeric Varible")
+df_nu = dfs.select_dtypes([np.number]) 
+df_nu.apply(var_summary).T
+# Everything looks fine. 
+#%%
+# Delete this when you start coding and analyzing:
+# I have set up the data set, along with the summary of all the variables. We need some plots of the data for visulization next. 
